f595ca2bb6b968d19e8c5f35bb8b117e983b7ece
Joshua Miertschin <the9rtyt@gmail.com>
2024-12-07 20:28:10 -0600

    bring up Memory view

--- src/vMem/MEMORY_ENTRY.java
+++ src/vMem/MEMORY_ENTRY.java
@@ -1,11 +1,20 @@
 package vMem;
 
 public class MEMORY_ENTRY {
-	
+
 	protected Process proc;
 	protected int val;
+
 	public MEMORY_ENTRY(Process proc, int val) {
-		this.proc=proc;
-		this.val=val;
+		this.proc = proc;
+		this.val = val;
+	}
+
+	@Override
+	public String toString() {
+		if (proc == null)
+			return "invalid";
+		else
+			return proc.getID() + "." + val;
 	}
 }
